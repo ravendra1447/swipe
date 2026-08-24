d188966bec38b0f0459e575523ca58a5b884764c
Admin <ravendra8957370964@gmail.com>
2026-08-24 18:33:40 +0530

    updatew

--- routes/eway_bills.js
+++ routes/eway_bills.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const db = require('../database');
 
 router.get('/', (req, res) => {
   db.query('SELECT * FROM eway_bills', (err, results) => {
